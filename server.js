a78d6ce25c5e06886bc52663fbe07a28c0a691d5
Hakan Akgül <hakanakgul34@gmail.com>
2017-01-26 14:04:48 +0300

    tarih ve saat ekleme

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ var http = require('http');
 var host = '192.168.1.100';
 var port = '80';
 var server = http.createServer(function(request, response) {
-    console.log("gelen istek : " + request.url);
+    console.log(zaman() + "gelen istek : " + request.url);
     response.writeHead(200, { 'Content-type': 'text plain' });
     response.end("merhaba dünya");
 });
 
 server.listen(port, host, function() {
-    console.log('Dinlenen  : ' + host + ':' + port);
+    console.log(zaman() + ' : Dinlenen  : ' + host + ':' + port);
 
 });
+
+function zaman() {
+
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
+
+    return datetime;
+}
