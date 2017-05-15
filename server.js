91cdab524e7c7e9fc12dec3481eb7c902c6df811
Hakan Akgül <hakanakgul34@gmail.com>
2017-05-15 18:16:07 +0300

    Revert "tarih ve saat ekleme"
    This reverts commit a78d6ce25c5e06886bc52663fbe07a28c0a691d5.

--- server.js
+++ server.js
@@ -2,25 +2,12 @@ var http = require('http');
 var host = '192.168.1.100';
 var port = '80';
 var server = http.createServer(function(request, response) {
-    console.log(zaman() + "gelen istek : " + request.url);
+    console.log("gelen istek : " + request.url);
     response.writeHead(200, { 'Content-type': 'text plain' });
     response.end("merhaba dünya");
 });
 
 server.listen(port, host, function() {
-    console.log(zaman() + ' : Dinlenen  : ' + host + ':' + port);
+    console.log('Dinlenen  : ' + host + ':' + port);
 
 });
-
-function zaman() {
-
-    var currentdate = new Date();
-    var datetime = currentdate.getDate() + "/" +
-        (currentdate.getMonth() + 1) + "/" +
-        currentdate.getFullYear() + " @ " +
-        currentdate.getHours() + ":" +
-        currentdate.getMinutes() + ":" +
-        currentdate.getSeconds();
-
-    return datetime;
-}
